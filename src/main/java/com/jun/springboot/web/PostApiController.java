869cf830dcf8c85ba579649c0f41ae61bd146b51
Yejun Park <jun020216@naver.com>
2022-03-27 00:27:31 +0900

    [FEAT] Posts 등록

---
+++ src/main/java/com/jun/springboot/web/PostApiController.java
@@ -0,0 +1,20 @@
+package com.jun.springboot.web;
+
+import com.jun.springboot.service.posts.PostsService;
+import com.jun.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}

29e235f8824c18c8cfe11eadeccd6e12e253af16
Yejun Park <jun020216@naver.com>
2022-04-02 14:00:16 +0900

    [FEAT] create page

--- src/main/java/com/jun/springboot/web/IndexController.java
+++ src/main/java/com/jun/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.jun.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")    // 머스테치에 url 매핑
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
